a8e0c1e8e071399c532fddda267e7093f4f0c14c
StephaneLay <stephane.lay@hotmail.fr>
2025-05-21 10:57:06 +0200

    increment bugfix

--- public/index.php
+++ public/index.php
@@ -10,13 +10,12 @@ require_once __DIR__ . "/../vendor/autoload.php";
 session_start();
 
 $cart = new Cart();
+$cart->addProductToCart();
 $header = new Navbar("/", "cart.php",$cart->getProductNumber());
 $productList = new ProductList();
 
 
 $productList->render();
-$cart->addProductToCart();
 
-// PROBLEME==== LE COMPTEUR S'INCREMENTE POUR RIEN DES QU'ON CHARGE UNE PAGE !!
-// IL FAUDRAIT PEUT ETRE REPENSER LE SYSTEME POUR CONTENIR LES INFORMATIONS DIFFEREMENT
+
 //fonction recherche!!!
